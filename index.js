41873e1d6d86054e054cf7ffc23258a4f84c8c17
Ferran Bals <fbals@uoc.edu>
2023-04-20 11:23:43 +0200

    endpoints test

--- index.js
+++ index.js
@@ -56,5 +56,260 @@ app.get("/", function (req, res) {
   res.send(response);
 });
 
+//Alice File
+app.get("/file/alice", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("alice", testSchema, "alice");
+  try {
+    const testDocs = await Test.find(); // Find all documents in "alice" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Alice - File",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log("Error al obtener documentos de la colección alice", error);
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección alice",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Alice - Minor File
+app.get("/file/minor", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("minor", testSchema, "minor");
+  try {
+    const testDocs = await Test.find(); // Find all documents in "alice" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Minor - File",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log("Error al obtener documentos de la colección minor", error);
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección minor",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Bob File
+app.get("/file/bob", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("bob", testSchema, "bob");
+  try {
+    const testDocs = await Test.find(); // Find all documents in "bob" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "API REST working.",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log("Error al obtener documentos de la colección bob", error);
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección bob",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Returns Alice enrollments
+app.get("/enrollments/alice", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model(
+    "aliceEnrollments",
+    testSchema,
+    "aliceEnrollments"
+  );
+  try {
+    const testDocs = await Test.find(); // Find all documents in "alice" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Alice - File",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log(
+      "Error al obtener documentos de la colección aliceEnrollments",
+      error
+    );
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección aliceEnrollments",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Returns Minor enrollments
+app.get("/enrollments/minor", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model(
+    "minorEnrollments",
+    testSchema,
+    "minorEnrollments"
+  );
+  try {
+    const testDocs = await Test.find(); // Find all documents in "minorEnrollments" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Alice - File",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log(
+      "Error al obtener documentos de la colección minorEnrollments",
+      error
+    );
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección minorEnrollments",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Returns Bob enrollments
+app.get("/enrollments/bob", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("bobEnrollments", testSchema, "bobEnrollments");
+  try {
+    const testDocs = await Test.find(); // Find all documents in "bobEnrollments" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Alice - File",
+      data: testDocs[0], // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log(
+      "Error al obtener documentos de la colección bobEnrollments",
+      error
+    );
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección bobEnrollments",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Returns Alice grades
+app.get("/grades", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("aliceMarks", testSchema, "aliceMarks");
+  try {
+    const testDocs = await Test.find({}); // Find all documents in "aliceMarks" collection
+    response = {
+      error: false,
+      code: 200,
+      message: "Alice - File",
+      data: testDocs, // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log(
+      "Error al obtener documentos de la colección aliceMarks",
+      error
+    );
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección aliceMarks",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
+//Return the convos of a particular subject
+
+app.get("/subject/", async (req, res) => {
+  // Add Mongoose model for "alice" collection
+
+  const Test = mongoose.model("aliceMarks", testSchema, "aliceMarks");
+  try {
+    const subjects = await Test.find();
+
+    const subject = subjects.find(
+      (obj) => obj.O[0].P.codAsignatura === req.query.codAsignatura
+    );
+
+    const subjectConvos = subject.O.map((item) => item.P);
+
+    response = {
+      error: false,
+      code: 200,
+      message: "parko - File",
+      asignatura: subjectConvos, // Add the document found to the response
+    };
+    console.log("Conectado y obtenida info");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(response);
+  } catch (error) {
+    console.log(
+      "Error al obtener documentos de la colección aliceMarks",
+      error
+    );
+    response = {
+      error: true,
+      code: 500,
+      message: "Error al obtener documentos de la colección aliceMarks",
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(500).send(response);
+  }
+});
+
 // Export the Express API
 module.exports = app;
